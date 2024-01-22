b61652ae13c811a9ff5848f86479fd70175444fa
Aymen EZZAYER <aymen.ezzayer@epita.fr>
2024-01-22 22:41:44 +0100

    ✨ Add pagination into URL Parameters

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
 import CompanyCard from './components/CompanyCard';
 
 export function capitalize(string) {
@@ -8,13 +9,24 @@ export function capitalize(string) {
 }
 
 export default function Home() {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+
+    const basePage = searchParams.has('page') ? parseInt(searchParams.get('page'), 10) : 1;
+
     const companyAPI = 'https://recherche-entreprises.api.gouv.fr';
     const companyGroupQuery = 'categorie_entreprise=PME%2CETI';// %2CGE"
 
     const [retryTrigger, setRetryTrigger] = useState(false);
     const [loading, setLoading] = useState(true);
     const [companies, setCompanies] = useState([]);
-    const [page, setPage] = useState(1);
+    const [page, setPage] = useState(basePage);
+
+    const updateURLParams = useCallback(() => {
+        const params = new URLSearchParams(searchParams);
+        params.set('page', page);
+        router.push(`?${params.toString()}`);
+    }, [page, searchParams, router]);
 
     useEffect(() => {
         setCompanies([]);
@@ -29,24 +41,26 @@ export default function Home() {
             .catch(() => {
                 setRetryTrigger(!retryTrigger);
             });
-    }, [page, retryTrigger]);
+
+        updateURLParams();
+    }, [page, retryTrigger, updateURLParams]);
 
     return (
         <main>
             <div className="mt-10 flex justify-center items-center flex-col gap-5">
                 <div className="flex justify-center items-center gap-10">
                     <button
-                      disabled={page == 1}
-                      className="glass shadowy-button w-36 flex justify-center items-center"
-                      onClick={() => {
+                        disabled={page == 1}
+                        className="glass shadowy-button w-36 flex justify-center items-center"
+                        onClick={() => {
                             setPage(page - 1);
                         }}
                     >
                         Previous
                     </button>
                     <button
-                      className="glass shadowy-button w-36 flex justify-center items-center"
-                      onClick={() => {
+                        className="glass shadowy-button w-36 flex justify-center items-center"
+                        onClick={() => {
                             setPage(page + 1);
                         }}
                     >
