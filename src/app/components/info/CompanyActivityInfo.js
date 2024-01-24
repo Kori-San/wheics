dc4cfb22c83aa728f4801a3b85962fc1f1ae6bc3
a-y-men <aymen@layan.eu>
2024-01-24 11:16:00 +0100

    ♻️ Make info show title even if data is unknown

--- src/app/components/info/CompanyActivityInfo.js
+++ src/app/components/info/CompanyActivityInfo.js
@@ -1,17 +1,18 @@
 import { FcFactory } from "react-icons/fc";
 
 import { nomenclatureActivitesFrancaise } from "@/app/data/nomenclatureActivitesFrancaise"
+import { unknownString } from "@/app/data/globalWording";
 
 export default function CompanyActivityInfo({ NAFCode }) {
-    const activity = nomenclatureActivitesFrancaise[NAFCode];
+    const textDisplayed = NAFCode ? nomenclatureActivitesFrancaise[NAFCode] : unknownString;
 
     return (
-        <div className="text-xs flex items-center gap-1 " title={activity}>
+        <div className="text-xs flex items-center gap-1 " title={textDisplayed}>
             <div className=" w-5 flex justify-center items-center">
                 <FcFactory size={15} />
             </div>
             <div className="overflow-hidden text-ellipsis whitespace-nowrap">
-                {activity ?? unknownString}
+                {textDisplayed}
             </div>
         </div>
     )
--- src/app/components/info/CompanyAddressInfo.js
+++ src/app/components/info/CompanyAddressInfo.js
@@ -2,13 +2,15 @@ import { unknownString } from "@/app/data/globalWording"
 import { FaLocationDot } from "react-icons/fa6"
 
 export default function CompanyAddressInfo({ address }) {
+    const textDisplayed = address ? address : unknownString;
+
     return (
-        <div className="text-xs flex items-center gap-1 " title={address}>
+        <div className="text-xs flex items-center gap-1 " title={textDisplayed}>
             <div className=" w-5 flex justify-center items-center">
                 <FaLocationDot size={15} style={{ fill: 'red' }} />
             </div>
             <div className="overflow-hidden text-ellipsis whitespace-nowrap">
-                {address ?? unknownString}
+                {textDisplayed}
             </div>
         </div>
     )
