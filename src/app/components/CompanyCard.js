4b0ac51136712c1d92c1e4f43f893bcbde6713e3
a-y-men <aymen@layan.eu>
2024-01-24 11:09:17 +0100

    💄 Make Company Cards translate less in the Y Axis

--- src/app/components/CompanyCard.js
+++ src/app/components/CompanyCard.js
@@ -6,7 +6,7 @@ import CompanyActivityInfo from './info/CompanyActivityInfo';
 
 export default function CompanyCard({ company }) {
     return (
-        <a className="flex justify-center items-center hover:-translate-y-2 transition ease-in-out duration-150" href={`/company/${company.siren}`} key={`${company.siren}-${company.siege.siret}`}>
+        <a className="flex justify-center items-center hover:-translate-y-1.5 transition ease-in-out duration-150" href={`/company/${company.siren}`} key={`${company.siren}-${company.siege.siret}`}>
             <div className=" bg-gray-200 hover:bg-gray-300 transition ease-in-out duration-150 rounded-lg w-11/12 h-48 px-3 pt-5">
                 <CompanyNameInfo name={company.nom_complet} />
                 <hr className="my-2 w-3/4 h-px border-none bg-slate-600" />
