1bebc18d3069b2c51b232b0ae75c74ad92e2f701
a-y-men <aymen@layan.eu>
2024-01-24 11:12:46 +0100

    🐛 Fix EmployeeInfo not showing full text on hover

--- src/app/components/info/CompanyEmployeeInfo.js
+++ src/app/components/info/CompanyEmployeeInfo.js
@@ -4,19 +4,15 @@ import { trancheEffectifSalarie } from "@/app/data/trancheEffectifSalarie"
 import { unknownString } from "@/app/data/globalWording"
 
 export default function CompanyEmployeeInfo({ workforceBracket }) {
-    console.log(workforceBracket);
+    const textDisplayed = workforceBracket ? trancheEffectifSalarie[workforceBracket] : unknownString;
 
     return (
         <div className="flex flex-row items-center text-xs gap-1">
             <div className=" w-5 flex justify-center items-center">
                 <FcConferenceCall size={18} />
             </div>
-            <div className="overflow-hidden text-ellipsis whitespace-nowrap">
-                {
-                    workforceBracket
-                        ? `${trancheEffectifSalarie[workforceBracket]}`
-                        : unknownString
-                }
+            <div className="overflow-hidden text-ellipsis whitespace-nowrap" title={textDisplayed}>
+                {textDisplayed}
             </div>
         </div>
     )
