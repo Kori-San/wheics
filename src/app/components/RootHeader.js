fd83122f670a45f558fb5d86adb6e97112f6aa67
Aymen EZZAYER <aymen.ezzayer@epita.fr>
2024-01-28 23:45:46 +0100

    ✨ Make query url persistent

--- src/app/components/RootHeader.js
+++ src/app/components/RootHeader.js
@@ -1,17 +1,23 @@
 import Image from 'next/image';
+import { useSearchParams } from 'next/navigation';
 
 export default function RootHeader({ setSearchQuery, setPage }) {
+    const searchParams = useSearchParams();
+    const params = new URLSearchParams(searchParams);
+
     return (
         <div className="m-8 flex flex-col md:flex-row justify-center gap-5 items-center">
-            <div className="flex flex-row rounded bg-slate-200">
+            <div className="flex flex-row rounded bg-gray-300">
                 <div className="px-2 py-1">
                     <a href="/">
                         <Image src="/images/wheics_logo_transparent.png" width={32} height={32} alt="Logo" />
                     </a>
                 </div>
                 <input
+                    type="text"
                     className=" w-96 focus:bg-gray-100 focus:text-gray-600 bg-white transition duration-300 ease-in-out px-2 rounded-e searchBar text-gray-400"
                     placeholder="SIREN, Dénomination ou Dirigeant"
+                    defaultValue={params.get('q') || ''}
                     onChange={(event) => {
                         setSearchQuery(event.target.value);
                         setPage(1);
