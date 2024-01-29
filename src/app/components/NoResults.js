6a08598fe114a105c86c39fc1c483129669c8e48
a-y-men <aymen@layan.eu>
2024-01-29 17:16:17 +0100

    📱 Make NoResults responsive

--- src/app/components/NoResults.js
+++ src/app/components/NoResults.js
@@ -5,7 +5,7 @@ export default function NoResults() {
     return (
         <div className="h-[400px] my-28 flex justify-center items-center flex-col">
             <GiFedora size={350} style={{ fill: 'white' }} />
-            <p className="text-5xl text-gray-300 font-bold">
+            <p className=" text-3xl text-center md:text-4xl mx-5 lg:text-5xl text-gray-300 font-bold">
                 {noResults}
             </p>
         </div>
