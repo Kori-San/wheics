9540324c197f27ff01f4a84890b38b6777c89231
Aymen EZZAYER <aymen.ezzayer@epita.fr>
2024-01-30 01:39:56 +0100

    💄 Update style of keyboardButton and add className prop

--- src/app/components/button/KeyboardButton.js
+++ src/app/components/button/KeyboardButton.js
@@ -1,15 +1,19 @@
 export default function KeyboardButton({
-    disabled = false, onClick = undefined, message, label = undefined,
+    className,
+    disabled = false,
+    onClick = undefined,
+    message,
+    label = undefined,
 }) {
     return (
         <button
             type="button"
             disabled={disabled}
-            className="keyboardButton"
+            className={`keyboardButton ${className}`}
             onClick={onClick}
             aria-label={label}
         >
-            <span>{message}</span>
+            <span className="flex px-2 flex-row gap-2">{message}</span>
         </button>
 
     );
