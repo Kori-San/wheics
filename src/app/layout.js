9564420ab0322f50167824f7886d8ad9d7a13d91
Kori-San <aymen@ezzayer.net>
2024-02-02 21:03:46 +0100

    📈 Add Vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
 import './style/globals.css';
 import './style/loader.css';
@@ -14,7 +15,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="fr">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                {children}
+                <Analytics />
+            </body>
         </html>
     );
 }
