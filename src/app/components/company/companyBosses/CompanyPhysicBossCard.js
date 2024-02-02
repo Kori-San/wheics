dfdc97520ee87045ba2506383ce0af242607c10a
Kori-San <aymen@ezzayer.net>
2024-02-02 22:34:53 +0100

    ✨ Add click on PhysicBosses to search by name

--- src/app/components/company/companyBosses/CompanyPhysicBossCard.js
+++ src/app/components/company/companyBosses/CompanyPhysicBossCard.js
@@ -8,7 +8,10 @@ export default function CompanyPhysicBossCard({ dirigeant }) {
     }
 
     return (
-        <div className="hover:-translate-y-1 transition ease-in-out duration-150 bg-gray-300 p-2 max-w-80 h-36 rounded-lg">
+        <a
+            href={`/?q=${encodeURIComponent(`${dirigeant.prenoms} ${dirigeant.nom}`)}`}
+            className="hover:-translate-y-1 transition ease-in-out duration-150 hover:bg-gray-400 bg-gray-300 p-2 max-w-80 h-36 rounded-lg"
+        >
             <div className="bg-gray-600 mb-2 flex gap-1 text-gray-200 items-center flex-row p-2 rounded">
                 <FcBusinessman size={24} />
                 <p>{capitalize(dirigeant.type_dirigeant)}</p>
@@ -18,6 +21,6 @@ export default function CompanyPhysicBossCard({ dirigeant }) {
                 <p className="text-ellipsis overflow-hidden whitespace-nowrap text-xs" title={dirigeant.prenoms}>{`Prénom(s): ${dirigeant.prenoms}`}</p>
                 <p className="ttext-ellipsis overflow-hidden whitespace-nowrap text-xs" title={dirigeant.qualite}>{`Qualité: ${dirigeant.qualite ?? unknownString}`}</p>
             </div>
-        </div>
+        </a>
     );
 }
