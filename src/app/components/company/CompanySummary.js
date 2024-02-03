e30108a44fa1c85a19624c6ea730816ce96cc87c
Kori-San <aymen@ezzayer.net>
2024-02-03 05:38:41 +0100

    ✨ Add SimilarSearch

--- src/app/components/company/CompanySummary.js
+++ src/app/components/company/CompanySummary.js
@@ -3,6 +3,7 @@ import CompanySimpleSummaryInfo from './companySummary/CompanySimpleSummaryInfo'
 import CompanyAddressSummaryInfo from './companySummary/CompanyAddressSummaryInfo';
 import CompanyEmployeeSummaryInfo from './companySummary/CompanyEmployeeSummaryInfo';
 import CompanyActivitySummaryInfo from './companySummary/CompanyActivitySummaryInfo';
+import CompanyFindSimilarSummaryInfo from './companySummary/CompanyFindSimilarSummaryInfo';
 
 export default function CompanySummary({ company }) {
     const address = company.siege.geo_adresse ?? company.siege.adresse;
@@ -16,13 +17,20 @@ export default function CompanySummary({ company }) {
                 siren={company.siren}
             />
             <CompanyAddressSummaryInfo name={name} address={address} />
+            <CompanyEmployeeSummaryInfo
+                workforceBracket={company.tranche_effectif_salarie}
+                year={company.annee_tranche_effectif_salarie}
+            />
             <CompanyActivitySummaryInfo
                 nafCode={company.activite_principale}
                 section={company.section_activite_principale}
             />
-            <CompanyEmployeeSummaryInfo
+            <hr />
+            <CompanyFindSimilarSummaryInfo
+                name={name}
                 workforceBracket={company.tranche_effectif_salarie}
-                year={company.annee_tranche_effectif_salarie}
+                category={company.categorie_entreprise.toLowerCase()}
+                section={company.section_activite_principale}
             />
         </div>
     );
---
+++ src/app/components/company/companySummary/CompanyFindSimilarSummaryInfo.js
@@ -0,0 +1,30 @@
+import { FcBinoculars, FcLink } from 'react-icons/fc';
+
+export default function CompanyFindSimilarSummaryInfo({
+    name,
+    workforceBracket,
+    category,
+    section,
+}) {
+    return (
+        <a
+            href={`/?sections=${section}&workforce=${workforceBracket}&categories=${category}`}
+            target="_blank"
+            className="group bg-gray-300 hover:bg-slate-700 p-3 rounded-lg transition-all ease-in-out hover:translate-x-1 duration-150 text-xl flex items-center gap-1"
+        >
+            <div className="group-hover:mx-1 flex justify-center items-center w-6">
+                <div className="transition-all duration-150 ease-in-out group-hover:w-full w-0 flex justify-center items-center">
+                    <FcLink size={24} />
+                </div>
+                <div className="transition-all duration-150 ease-in-out group-hover:w-0 w-full flex justify-center items-center">
+                    <FcBinoculars size={20} />
+                </div>
+            </div>
+            <div className="transition-all text-slate-700 group-hover:text-gray-300 duration-300 ease-in-out text-ellipsis whitespace-nowrap overflow-hidden">
+                Rechercher une entreprise similaire à
+                {' '}
+                <b>{name}</b>
+            </div>
+        </a>
+    );
+}
