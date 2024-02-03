433fb45cb5734f7eed0bade77ccccebdbddf3443
Kori-San <aymen@ezzayer.net>
2024-02-03 05:45:05 +0100

    ✨ Employee Summary Info now redircets to search with same Workforce

--- src/app/components/company/companySummary/CompanyEmployeeSummaryInfo.js
+++ src/app/components/company/companySummary/CompanyEmployeeSummaryInfo.js
@@ -1,4 +1,4 @@
-import { FcConferenceCall } from 'react-icons/fc';
+import { FcConferenceCall, FcLink } from 'react-icons/fc';
 
 import trancheEffectifSalarie from '@/app/data/trancheEffectifSalarie';
 import { unknownString, unknownDate } from '@/app/data/globalWording';
@@ -9,13 +9,22 @@ export default function CompanyEmployeeSummaryInfo({ workforceBracket, year }) {
         : unknownString;
 
     return (
-        <div className="transition ease-in-out hover:translate-x-1 duration-300 flex flex-row items-center text-xl gap-1">
-            <div className=" w-6 flex justify-center items-center">
-                <FcConferenceCall size={20} />
+        <a
+            href={`/?workforce=${workforceBracket}`}
+            target="_blank"
+            className="group transition-all ease-in-out hover:translate-x-1 duration-150 flex flex-row items-center gap-1 text-xl"
+        >
+            <div className="group-hover:mx-1 flex justify-center items-center w-6">
+                <div className="transition-all duration-150 ease-in-out group-hover:w-full w-0 flex justify-center items-center">
+                    <FcLink size={24} />
+                </div>
+                <div className="transition-all duration-150 ease-in-out group-hover:w-0 w-full flex justify-center items-center">
+                    <FcConferenceCall size={20} />
+                </div>
             </div>
             <div className="text-ellipsis whitespace-nowrap overflow-hidden">
                 {textDisplayed}
             </div>
-        </div>
+        </a>
     );
 }
